362ac9b9c47d9de177f7ffe9a1cb3a8e4ec63143
Shyryn <shyrynakylbaeva@icloud.com>
2025-12-08 17:10:04 +0500

    Initial Commit

---
+++ ExtracurricularActivityApp/ExtracurricularActivityAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExtracurricularActivityAppApp.swift
+//  ExtracurricularActivityApp
+//
+//  Created by Shyryn Akylbaeva on 08.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct ExtracurricularActivityAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
